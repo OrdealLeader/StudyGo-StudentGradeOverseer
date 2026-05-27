c5a68a7ea65346c9c840afafd1602870ccf28246
Juels <tjuelsraphael@gmail.com>
2026-05-28 00:22:23 +0800

    connected fragment_results xml to results fragments java

--- app/src/main/java/com/example/studygo_studentgradeoverseer/ResultsFragment.java
+++ app/src/main/java/com/example/studygo_studentgradeoverseer/ResultsFragment.java
@@ -5,11 +5,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.example.studygo_studentgradeoverseer.databinding.FragmentResultsBinding;
 
 public class ResultsFragment extends Fragment {
+
+    private FragmentResultsBinding binding;
+
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(android.R.layout.simple_list_item_1, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentResultsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Initialize your views and logic here
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
